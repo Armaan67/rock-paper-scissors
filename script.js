1b0c6ef0ec4f44304dc9bf60b2464ed8c0b47073
Armaan67 <117089919+Armaan67@users.noreply.github.com>
2024-11-18 01:58:21 -0500

    Implement function for random computer choice

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World!")
+function getComputerChoice() {
+    let numChoice = Math.floor(Math.random()*3+1);
+    let compChoice;
+    switch (numChoice) {
+        case 1:
+            compChoice = "Rock"
+            break;
+        case 2:
+            compChoice = "Paper"
+            break;
+        case 3:
+            compChoice = "Scissors"
+            break;
+    }
+    return compChoice
+}
+
